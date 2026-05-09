58e7436b65b552d189f7f40db0f2fdc08b4d706e
Jean Peralta <jean.c.peralta@proton.me>
2026-05-09 08:40:09 -0400

    Create install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+sudo pacman -S hyprland xorg-xwayland \
+    xdg-desktop-portal-hyprland \
+    hyprpaper hyprlock hypridle \
+    hyprpolkitagent \
+    waybar wofi dunst \
+    qt5-wayland qt6-wayland \
+    kitty git \
+    sddm \
+    wl-clipboard \
+    noto-fonts ttf-jetbrains-mono-nerd \
+    thunar \
+    thunar-volman \
+    thunar-archive-plugin \
+    thunar-media-tags-plugin \
+    gvfs \
+    gvfs-mtp \
+    gvfs-gphoto2 \
+    tumbler \
+    ffmpegthumbnailer \
+    file-roller
+
+systemctl enable sddm
