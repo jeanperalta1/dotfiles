63bbeaeab03062f7aadb1733de8198bd59914e7d
Jean Peralta <jean.c.peralta@proton.me>
2026-08-18 14:06:14 -0400

    Add shortcut to open html files

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<leader>bo", function()
+  vim.fn.jobstart({ "xdg-open", vim.fn.expand("%:p") }, { detach = true })
+end, { desc = "Open file in browser" })
